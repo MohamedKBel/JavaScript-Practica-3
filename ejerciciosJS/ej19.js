de3eaa2dbf8328a7e735973134c2cad495e96c04
Mohamed KB <mohabel98@gmail,com>
2022-11-22 21:42:37 +0100

    ejercicios hecho

--- ejerciciosJS/ej19.js
+++ ejerciciosJS/ej19.js
@@ -1,13 +1,13 @@
 let Trabajo = prompt("Tienes trabajo hoy?");
 let BuenTiempo = prompt("Hace buen tiempo?");
 
-if (Trabajo == si || Trabajo == SI || Trabajo == sI || Trabajo == Si){
+if (Trabajo == 'si' || Trabajo == 'SI' || Trabajo == 'sI' || Trabajo == 'Si'){
     Trabajo = true;
 }else{
     Trabajo = false;
 }
 
-if (BuenTiempo == si || BuenTiempo == SI || BuenTiempo == sI || BuenTiempo == Si){
+if (BuenTiempo == 'si' || BuenTiempo == 'SI' || BuenTiempo == 'sI' || BuenTiempo == 'Si'){
     BuenTiempo = true;
 }else{
     BuenTiempo = false;
@@ -15,6 +15,9 @@ if (BuenTiempo == si || BuenTiempo == SI || BuenTiempo == sI || BuenTiempo == Si
 
 if(BuenTiempo === true && Trabajo === false){
     console.log("Puedes ir a la playa.");
-}else{
-    console.log("No puedes ir a la playa.");
+}else if(BuenTiempo === false)
+{
+    console.log("No puedes ir a la playa, hoy toca estar en casita.");
+}else if (Trabajo === true) {
+    console.log("Hoy toca ir a trabajar.");
 }
